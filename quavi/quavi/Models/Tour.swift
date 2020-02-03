38e1b4f728651ae2ea368f57effe031192cb39c4
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-02-03 18:00:31 -0500

    creates generateNaviationRouteOptions func in Tour model

--- quavi/quavi/Models/Tour.swift
+++ quavi/quavi/Models/Tour.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import MapboxDirections
+import MapboxCoreNavigation
 
 struct Tour {
     let name: String
@@ -16,8 +17,29 @@ struct Tour {
     
     static let dummyData: Tour = Tour(name: "DummyHistory", category: "History", stops: POI.pointsOfinterest)
     
-    func generateWaypoints(from tour: Tour) -> [Waypoint] {
+    private func generateWaypoints(from tour: Tour) -> [Waypoint] {
         let waypoints = tour.stops.map { $0.waypoint }
         return waypoints
     }
+    
+    func generateNaviationRouteOptions(from tour: Tour, initialLocation: CLLocationCoordinate2D?, navigationType: MBDirectionsProfileIdentifier, completion: @escaping ((Result<NavigationRouteOptions, MapboxError>) -> ())) {
+        
+        //TODO: Handle else statement
+        guard let initialLocation = initialLocation else { return }
+        
+        let initialWaypoint = Waypoint(coordinate: initialLocation, coordinateAccuracy: -1, name: "Initial Location")
+        let stops = generateWaypoints(from: tour)
+        
+        var waypoints = [Waypoint]()
+        waypoints.append(initialWaypoint)
+        waypoints.append(contentsOf: stops)
+        
+        let options = NavigationRouteOptions(waypoints: waypoints, profileIdentifier: navigationType)
+        
+        completion(.success(options))
+        completion(.failure(MapboxError.generatingOptionsError))
+    }
+    
+    //TODO: make generateRoute(from options:...) func in MapVC
+    
 }
