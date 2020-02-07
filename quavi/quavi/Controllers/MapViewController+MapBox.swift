cdeec2c336f15dc7d2dd8f5fa918b37b26521f64
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2020-02-07 16:47:07 -0500

    Adds setStartNavigationButtonConstraints func to MapViewController+Constraints

--- quavi/quavi/Controllers/MapViewController+MapBox.swift
+++ quavi/quavi/Controllers/MapViewController+MapBox.swift
@@ -47,7 +47,7 @@ extension MapViewController: MGLMapViewDelegate {
             }
         }
     }
-    
+
     func addMapAnnotations(from selectedRoute: Route) {
         //Creating points as an array of MGLPointAnnotations then adding as annotations in mapView
         //TODO: Remove annotation for first annotation
