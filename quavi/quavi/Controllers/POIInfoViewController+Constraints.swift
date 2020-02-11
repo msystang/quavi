12dc13b53724e449b087832a6df94f9a2436c4b5
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2020-02-11 16:33:12 -0500

    Adds constraints to ContinueButton in POIInfoViewController

--- quavi/quavi/Controllers/POIInfoViewController+Constraints.swift
+++ quavi/quavi/Controllers/POIInfoViewController+Constraints.swift
@@ -5,5 +5,18 @@
 //  Created by Mr Wonderful on 2/11/20.
 //  Copyright © 2020 Sunni Tang. All rights reserved.
 //
+import UIKit
 
-import Foundation
+extension POIInfoViewController{
+    
+    func addSubviews() {
+        view.addSubview(ContinueButton)
+    }
+    
+     func continueButtonConstraints(){
+        
+        ContinueButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([ContinueButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant:  -20), ContinueButton.centerYAnchor.constraint(equalTo: view.centerYAnchor), ContinueButton.heightAnchor.constraint(equalToConstant: 100), ContinueButton.widthAnchor.constraint(equalToConstant: 100)])
+    }
+
+}
--- quavi/quavi/Controllers/POIInfoViewController.swift
+++ quavi/quavi/Controllers/POIInfoViewController.swift
@@ -14,14 +14,18 @@ class POIInfoViewController: UIViewController {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
         button.setTitle("Next", for: .normal)
         button.setTitleColor(.purple, for: .normal)
+        button.layer.cornerRadius = button.frame.height / 2
+        button.layer.borderColor = #colorLiteral(red: 0.2046233416, green: 0.1999312043, blue: 0.1955756545, alpha: 1)
+        button.layer.borderWidth = 2
         button.addTarget(self, action: #selector(continueButtonPressed(_:)), for: .touchUpInside)
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
          setBackgroundColor()
+         addSubviews()
+         continueButtonConstraints()
     }
     
     private func setBackgroundColor(){
