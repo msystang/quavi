5acad561ec05237e405428010aea9d1adf51c4d9
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2020-02-11 16:09:21 -0500

    Creates button in POIInfoViewController

--- quavi/quavi/Controllers/POIInfoViewController.swift
+++ quavi/quavi/Controllers/POIInfoViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class POIInfoViewController: UIViewController {
 
+    lazy var button:UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        button.setTitle("Next", for: .normal)
+        button.setTitleColor(.purple, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
