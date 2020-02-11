9613d021b4161b940854605534569391498ac6d0
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2020-02-11 16:21:33 -0500

    Adds continueButtonPressed target func to continueButton

--- quavi/quavi/Controllers/POIInfoViewController.swift
+++ quavi/quavi/Controllers/POIInfoViewController.swift
@@ -14,15 +14,21 @@ class POIInfoViewController: UIViewController {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
         button.setTitle("Next", for: .normal)
         button.setTitleColor(.purple, for: .normal)
+        button.addTarget(self, action: #selector(continueButtonPressed(_:)), for: .touchUpInside)
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+         setBackgroundColor()
     }
     
+    private func setBackgroundColor(){
+        view.backgroundColor = .white
+    }
 
-
+    @objc func continueButtonPressed(_ sender: Any) {
+    
+    }
 }
