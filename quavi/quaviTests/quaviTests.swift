4e0f71a03f8abe39717b8546f692dab7c72db80d
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-02-13 17:49:23 -0500

    refactors unit tests to use Quick and Nimble cocoapods

--- quavi/quaviTests/quaviTests.swift
+++ quavi/quaviTests/quaviTests.swift
@@ -13,23 +13,69 @@ import Nimble
 
 @testable import quavi
 
-class quaviTests: XCTestCase {
+class quaviTests: QuickSpec {
 
+    override func spec() {
+        let testInitialLocation = CLLocationCoordinate2D(latitude: 40.7000, longitude: -74.0000)
+        let testPointA = POI(name: "Point A", isCellExpanded: false, lat: 40.7011, long: -74.0011, shortDesc: "Short Description A", longDesc: "Long Discription A", tableViewImage: UIImage(), popoverImage: UIImage())
+        let testPointB = POI(name: "Point B", isCellExpanded: false, lat: 40.7022, long: -74.0022, shortDesc: "Short Description B", longDesc: "Long Discription B", tableViewImage: UIImage(), popoverImage: UIImage())
+        
+        //MARK: - POI Model
+        //Arrange & Act: Create test POI above. POI model has a computed variable waypoint
+        describe("A POI") {
+            context("when instantiated with a latitude and longitude of type Double") {
+                it("should compute a waypoint property with coordinate.latitude and coordinate.longitude values of type CLLocationDegrees") {
+                    
+                //Assert: Assert that the waypoint is computed correctly by testing the original latitude to the latitude of the computed waypoint
+                   XCTAssert(testPointA.lat == testPointA.waypoint.coordinate.latitude, "Expected latitude of \(testPointA.lat) but got \(testPointA.waypoint.coordinate.latitude)")
+        
+        // Look into what Predicate types are to use Quick/Nimble language below
+        //                    expect(testPointA.lat).to(testPointA.waypoint.coordinate.latitude)
+                }
+            }
+        }
+        
+        //MARK: - Tour Model
+        describe("A tour") {
+            //Arrange: create test Tour
+            let testTour = Tour(name: "Name", category: "Category", stops: [testPointA, testPointB])
+            
+            context("when we apply generateNagivationRouteOptions") {
+                it("should return a NavigationRouteOption with properties such as waypoints that are all the POI of the tour") {
+                    //Act: using generateNavigationRouteOptions func on test data
+                    do {
+                        let testOptions = try Tour.generateNavigationRouteOptions(from: testTour, initialLocation: testInitialLocation, navigationType: .walking)
+                        //Assert: assert that we get the expected number of waypoints from test data (we should get 3: testPoint A, testPointB, testInitialLocation)
+                        expect(testOptions.waypoints.count).to(equal(3))
+                    } catch {
+                        fail("Could not generate nagivation route options: \(error)")
+                    }
+                }
+            }
+            
+        }
+        
+    }
+}
+
+/*
+class quaviTestsUsingXCTest: XCTest {
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     //MARK: - Tour Model
     func testGenerateNavigationRouteOptions() {
         //Arrange: create test POI, Tour, and initialLocation
+        let testInitialLocation = CLLocationCoordinate2D(latitude: 40.7000, longitude: -74.0000)
         let testPointA = POI(name: "Point A", isCellExpanded: false, lat: 40.7011, long: -74.0011, shortDesc: "Short Description A", longDesc: "Long Discription A", tableViewImage: UIImage(), popoverImage: UIImage())
         let testPointB = POI(name: "Point B", isCellExpanded: false, lat: 40.7022, long: -74.0022, shortDesc: "Short Description B", longDesc: "Long Discription B", tableViewImage: UIImage(), popoverImage: UIImage())
         let testTour = Tour(name: "Name", category: "Category", stops: [testPointA, testPointB])
-        let testInitialLocation = CLLocationCoordinate2D(latitude: 40.7000, longitude: -74.0000)
         
         //Act: using generateNavigationRouteOptions func on test data
         do {
@@ -49,5 +95,5 @@ class quaviTests: XCTestCase {
         //Assert: Assert that the waypoint is computed correctly by testing the original latitude to the latitude of the computed waypoint
         XCTAssert(testPointA.lat == testPointA.waypoint.coordinate.latitude, "Expected latitude of \(testPointA.lat) but got \(testPointA.waypoint.coordinate.latitude)")
     }
-    
 }
+*/
