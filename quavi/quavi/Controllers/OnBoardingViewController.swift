af507e1533ca11e30c10b3221b9064726305a4ec
aabudu16 <Aabudu16@yahoo.com>
2020-02-15 18:08:24 -0500

    Adds OnBoardingViewController.swift file

---
+++ quavi/quavi/Controllers/OnBoardingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OnBoardingViewController.swift
+//  quavi
+//
+//  Created by Mr Wonderful on 2/15/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
