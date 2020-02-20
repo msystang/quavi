380b7f19eb6aa4f3e92f40c4c7e38b1b0b08213b
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-02-20 16:12:20 -0500

    creates extension for strings for email and password validation

--- quavi/quavi/Utilities/Extensions/String+Validation.swift
+++ quavi/quavi/Utilities/Extensions/String+Validation.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+extension String {
+    
+    var isValidEmail: Bool {
+        
+        let validEmailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", validEmailRegEx)
+        return emailPredicate.evaluate(with: self)
+    }
+    
+    var isValidPassword: Bool {
+        
+        let validPasswordRegEx =  "[A-Z0-9a-z!@#$&*.-]{8,}"
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", validPasswordRegEx)
+        return passwordPredicate.evaluate(with: self)
+    }
+}
