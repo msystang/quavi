1120d08990658ff8a883ae83b4bd84cad7e70908
aabudu16 <Aabudu16@yahoo.com>
2020-02-20 16:13:44 -0500

    Creates QuaviOnBoardingDelegate protocol and adds an extension to QuaviOnBoardingDelegate protocol in order to make functions not required when conforming

--- quavi/quavi/Utilities/Protocols/ProtocolFile.swift
+++ quavi/quavi/Utilities/Protocols/ProtocolFile.swift
@@ -11,3 +11,17 @@ import Foundation
 protocol WaypointConfirmationViewControllerDelegate: NSObject {
     func proceedToNextLegInTour(_ controller: POIInfoViewController)
 }
+
+//onBoarding delegate
+ protocol QuaviOnBoardingDelegate: AnyObject {
+    
+    func quaviOnBoarding(_ quaviOnBoarding: OnBoarding, currentPage index: Int)
+    func quaviOnBoarding(_ quaviOnBoarding: OnBoarding, tapped index: Int)
+    
+}
+
+extension QuaviOnBoardingDelegate {
+    func quaviOnBoarding(_ quaviOnBoarding: OnBoarding, currentPage index: Int){}
+    
+    func quaviOnBoarding(_ quaviOnBoarding: OnBoarding, tapped index: Int){}
+}
--- quavi/quavi/Views/OnBoarding.swift
+++ quavi/quavi/Views/OnBoarding.swift
@@ -6,4 +6,8 @@
 //  Copyright © 2020 Sunni Tang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class OnBoarding: UIView {
+    
+}
