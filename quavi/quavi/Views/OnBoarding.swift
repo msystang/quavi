f5a809946381ee0175647218fc9dd49ca2460935
aabudu16 <Aabudu16@yahoo.com>
2020-02-20 17:17:02 -0500

    Adds containerView(scrollview) with constraints to OnBoarding.swift file

--- quavi/quavi/Views/OnBoarding.swift
+++ quavi/quavi/Views/OnBoarding.swift
@@ -9,5 +9,30 @@
 import UIKit
 
 class OnBoarding: UIView {
+     //MARK: -- Objects
+    lazy var containerView:UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.isPagingEnabled = true
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isUserInteractionEnabled = true
+        scrollView.isScrollEnabled = true
+        return scrollView
+    }()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: -- Private constraints
+    private func configureScrollView() {
+        self.addSubview(containerView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([containerView.topAnchor.constraint(equalTo: self.topAnchor), containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor), containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor), containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor)])
+    }
 }
