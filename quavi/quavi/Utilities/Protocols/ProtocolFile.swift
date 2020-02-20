fd006f62b208a04e65b2fe6b279f5a47f3960dd6
aabudu16 <Aabudu16@yahoo.com>
2020-02-20 16:26:32 -0500

    Adds QuaviOnBoardingDataSource protocol to project and creates an extension to QuaviOnBoardingDataSource to make some functions not required when conforming

--- quavi/quavi/Utilities/Protocols/ProtocolFile.swift
+++ quavi/quavi/Utilities/Protocols/ProtocolFile.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Sunni Tang. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 protocol WaypointConfirmationViewControllerDelegate: NSObject {
     func proceedToNextLegInTour(_ controller: POIInfoViewController)
@@ -24,4 +24,23 @@ extension QuaviOnBoardingDelegate {
     func quaviOnBoarding(_ quaviOnBoarding: OnBoarding, currentPage index: Int){}
     
     func quaviOnBoarding(_ quaviOnBoarding: OnBoarding, tapped index: Int){}
+}
+
+
+//onBoarding dataSource
+ protocol QuaviOnBoardingDataSource: AnyObject {
+    func quaviOnboardBackgroundColorFor(_ quickGrubOnBoarding: OnBoarding, atIndex index: Int) -> UIColor?
+    func quaviOnboardNumberOfPages(_ quickGrubOnBoarding: OnBoarding) -> Int
+    func quaviOnboardPageForIndex(_ quickGrubOnBoarding: OnBoarding, index: Int) -> QuaviOnBoardingPage?
+    func quaviOnboardViewForOverlay(_ quickGrubOnBoarding: OnBoarding) -> QuaviOnboardOverlay?
+    func quaviOnboardOverlayForPosition(_ quickGrubOnBoarding: OnBoarding, overlay: QuaviOnboardOverlay, for position: Double)
+    
+}
+
+//  extension to onBoarding dataSource to make functions not required when conforming
+extension QuaviOnBoardingDataSource{
+      func quaviOnboardOverlayForPosition(_ quickGrubOnBoarding: OnBoarding, overlay: QuaviOnboardOverlay, for position: Double) {}
+      func quaviOnboardViewForOverlay(_ quickGrubOnBoarding: OnBoarding) -> QuaviOnboardOverlay? {
+          return QuaviOnboardOverlay()
+      }
 }
