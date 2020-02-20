c8db108da457706ca486fc918960898b7b05ea17
aabudu16 <Aabudu16@yahoo.com>
2020-02-20 16:49:36 -0500

    Adds pageControl, continueButton, skipButton, prevLabel and nextLabel objects to QuaviOnboardOverlay.swift file

--- quavi/quavi/Views/QuaviOnboardOverlay.swift
+++ quavi/quavi/Views/QuaviOnboardOverlay.swift
@@ -9,5 +9,41 @@
 import UIKit
 
 class QuaviOnboardOverlay:UIView {
+    //MARK: -- objects
+    var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        return pageControl
+    }()
     
+    open var continueButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Continue", for: .normal)
+        button.contentHorizontalAlignment = .center
+        button.showsTouchWhenHighlighted = true
+        button.isHidden = true
+        button.isEnabled = false
+        return button
+    }()
+    
+    open var skipButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Skip", for: .normal)
+        button.contentHorizontalAlignment = .center
+        button.showsTouchWhenHighlighted = true
+        return button
+    }()
+    
+    open var prevLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Prev"
+        return label
+    }()
+    
+    open var nextLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Next"
+        return label
+    }()
 }
