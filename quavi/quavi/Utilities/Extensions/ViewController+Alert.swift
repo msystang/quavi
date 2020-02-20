081b819008e21ea001979bc1cafaa2873a17f512
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-02-20 16:12:37 -0500

    creates extension for viewControllers to showAlert func

--- quavi/quavi/Utilities/Extensions/ViewController+Alert.swift
+++ quavi/quavi/Utilities/Extensions/ViewController+Alert.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    public func showAlert(title: String?, message: String?) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { alert in }
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
