98e973004553438e78adee789351454705f9cb0f
aabudu16 <Aabudu16@yahoo.com>
2020-02-20 17:27:27 -0500

    Adds setUpAllPages func to create and design all onBoard pages based on the values inputted from the onBoardingVC

--- quavi/quavi/Views/OnBoarding.swift
+++ quavi/quavi/Views/OnBoarding.swift
@@ -29,13 +29,40 @@ class OnBoarding: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        configureScrollView()
+        setUpAllPages()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    //MARK: -- private func
+    //MARK: -- set up all pages of the onBoarding views 6
+    private func setUpAllPages() {
+        // un wrap the dataSource
+        if let dataSource = dataSource{
+            //get the count of pages from the dataSource when provided
+            pageCount = dataSource.quaviOnboardNumberOfPages(self)
+            // loop through the amount of count to populate the data for each page from the dataSource provided
+            for index in 0..<pageCount{
+                if let eachView = dataSource.quaviOnboardPageForIndex(self, index: index) {
+                    if let color = dataSource.quaviOnboardBackgroundColorFor(self, atIndex: index){
+                        eachView.backgroundColor = color
+                    // add to scroll view subview
+                    containerView.addSubview(eachView)
+                    // Create a frame to fit each view for there index by multiplying the frame by the amount of views it has
+                    var viewFrame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+                    viewFrame.origin.x = self.frame.width * CGFloat(index)
+                    eachView.frame = viewFrame
+                }
+                }
+            }
+            // this resizes the container ScrollView to be able to scroll to all the pages added.. Although all the views are on the screen, we wont be able to scroll to see see them.
+              containerView.contentSize = CGSize(width: self.frame.width * CGFloat(pageCount), height: self.frame.height)
+        }
+    }
     //MARK: -- Private constraints
     private func configureScrollView() {
         self.addSubview(containerView)
