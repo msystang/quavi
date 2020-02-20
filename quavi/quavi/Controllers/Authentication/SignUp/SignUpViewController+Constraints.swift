8a47c506dc9867fc1513c4b72826bbe194dfe07c
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-02-20 15:52:34 -0500

    adds constraints for objects in SignUpVC+constraints

--- quavi/quavi/Controllers/Authentication/SignUp/SignUpViewController+Constraints.swift
+++ quavi/quavi/Controllers/Authentication/SignUp/SignUpViewController+Constraints.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+import UIKit
+
+extension SignUpViewController {
+    // MARK: - Constraint Methods
+    func addSubviews() {
+        view.addSubview(titleLabel)
+        view.addSubview(signUpStackView)
+        view.addSubview(signUpButton)
+    }
+    
+    func addConstraints() {
+        setTitleLabelConstraints()
+        setSignUpStackViewConstraints()
+        setSignUpButtonConstraints()
+    }
+    
+    private func setTitleLabelConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            titleLabel.bottomAnchor.constraint(equalTo: signUpStackView.topAnchor, constant: 30),
+            titleLabel.leadingAnchor.constraint(equalTo: signUpStackView.leadingAnchor),
+            titleLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ])
+    }
+    
+    private func setSignUpStackViewConstraints() {
+        signUpStackView.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            signUpStackView.bottomAnchor.constraint(equalTo: view.centerYAnchor),
+            signUpStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signUpStackView.heightAnchor.constraint(lessThanOrEqualToConstant: 100),
+            emailTextField.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8),
+            passwordTextField.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8),
+        ])
+    }
+    
+    
+    private func setSignUpButtonConstraints() {
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            signUpButton.topAnchor.constraint(equalTo: signUpStackView.bottomAnchor, constant: 30),
+            signUpButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            signUpButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.5),
+            signUpButton.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            signUpButton.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+
+}
