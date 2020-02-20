e22cc18b7a386dcb112d91f5f31474988479974a
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-02-20 13:49:34 -0500

    creates objects for loginViewController

--- quavi/quavi/Controllers/Authentication/LoginViewController.swift
+++ quavi/quavi/Controllers/Authentication/LoginViewController.swift
@@ -9,10 +9,55 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    //MARK: - UI Objects
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Quavi"
+        return label
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Email"
+        return textField
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Password"
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        //TODO: Add target for loginButtonPressed func
+        return button
+    }()
+    
+    lazy var logInStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton])
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    lazy var createAccountButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Create New Account", for: .normal)
+        button.showsTouchWhenHighlighted = true
+        //TODO: Add target for createAccountButtonPressed func
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .white
+        addSubviews()
+        addConstraints()
         // Do any additional setup after loading the view.
     }
     
