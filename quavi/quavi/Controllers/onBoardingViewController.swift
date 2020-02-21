f9c2e96effa6e0801b3c3c5bc49c4d9b27ea1316
aabudu16 <Aabudu16@yahoo.com>
2020-02-21 13:11:28 -0500

    Adds onBoardingScrollView with constraints to onBoardingViewController

--- quavi/quavi/Controllers/onBoardingViewController.swift
+++ quavi/quavi/Controllers/onBoardingViewController.swift
@@ -9,11 +9,32 @@
 import UIKit
 
 class onBoardingViewController: UIViewController {
-
+//MARK:-- Objects
+    lazy var onBoardingScrollView: OnBoarding = {
+        let scrollView = OnBoarding()
+        scrollView.backgroundColor = .clear
+        return scrollView
+    }()
+    
+    //MARK:-- LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addSubView()
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+         configureScrollViewConstraints()
+    }
+    //MARK: -- private func
+    
+    private func addSubView(){
+        view.addSubview(onBoardingScrollView)
+    }
+    
+     private func configureScrollViewConstraints(){
+         onBoardingScrollView.translatesAutoresizingMaskIntoConstraints = false
+         NSLayoutConstraint.activate([onBoardingScrollView.topAnchor.constraint(equalTo: view.topAnchor), onBoardingScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor), onBoardingScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor) ,onBoardingScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+     }
 
 }
