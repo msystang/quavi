634d8ff921b60a6d0e10b5dd8e618a89a56cb74c
aabudu16 <Aabudu16@yahoo.com>
2020-02-21 12:46:35 -0500

    Adds onBoardingViewController.swift file to project

---
+++ quavi/quavi/Controllers/onBoardingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  onBoardingViewController.swift
+//  quavi
+//
+//  Created by Mr Wonderful on 2/21/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class onBoardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
