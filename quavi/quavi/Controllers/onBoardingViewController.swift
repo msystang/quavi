de6b6e795dd546aac05b4bb5744a5aa7c9cd2f03
aabudu16 <Aabudu16@yahoo.com>
2020-02-21 13:41:51 -0500

    conform to QuaviOnBoardingDataSource in onBoardingViewController

--- quavi/quavi/Controllers/onBoardingViewController.swift
+++ quavi/quavi/Controllers/onBoardingViewController.swift
@@ -22,10 +22,10 @@ class onBoardingViewController: UIViewController {
         super.viewDidLoad()
         addSubView()
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-         configureScrollViewConstraints()
+        configureScrollViewConstraints()
     }
     //MARK: -- private func
     
@@ -33,9 +33,39 @@ class onBoardingViewController: UIViewController {
         view.addSubview(onBoardingScrollView)
     }
     
-     private func configureScrollViewConstraints(){
-         onBoardingScrollView.translatesAutoresizingMaskIntoConstraints = false
-         NSLayoutConstraint.activate([onBoardingScrollView.topAnchor.constraint(equalTo: view.topAnchor), onBoardingScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor), onBoardingScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor) ,onBoardingScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
-     }
+    private func configureScrollViewConstraints(){
+        onBoardingScrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([onBoardingScrollView.topAnchor.constraint(equalTo: view.topAnchor), onBoardingScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor), onBoardingScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor) ,onBoardingScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+    }
+}
 
+extension onBoardingViewController: QuaviOnBoardingDataSource{
+    func quaviOnboardBackgroundColorFor(_ quickGrubOnBoarding: OnBoarding, atIndex index: Int) -> UIColor? {
+        
+        //TODO: set background color dynamically
+        switch index{
+        case 0:
+            return .white
+        case 1:
+            return .blue
+        case 2:
+            return .yellow
+        case 3:
+            return .green
+        default:
+            return .white
+        }
+    }
+    
+    func quaviOnboardNumberOfPages(_ quickGrubOnBoarding: OnBoarding) -> Int {
+        return 4
+    }
+    
+    func quaviOnboardPageForIndex(_ quickGrubOnBoarding: OnBoarding, index: Int) -> QuaviOnBoardingPage? {
+        let page = QuaviOnBoardingPage()
+        
+        //TODO: Set information to each onboarding page here
+        return page
+    }
+    
 }
