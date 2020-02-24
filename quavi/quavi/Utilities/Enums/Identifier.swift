4be514af31ca4963c79312e1afaf06d530b21284
aabudu16 <Aabudu16@yahoo.com>
2020-02-24 14:28:17 -0500

    Create Identifier.swift file in the Enums folder

---
+++ quavi/quavi/Utilities/Enums/Identifier.swift
@@ -0,0 +1,13 @@
+//
+//  Identifier.swift
+//  quavi
+//
+//  Created by Mr Wonderful on 2/24/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+enum Identifier {
+    case categoryCell
+}
