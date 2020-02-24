fe83228b6990e6ef7d02670521f3b8afe5d54d9a
aabudu16 <Aabudu16@yahoo.com>
2020-02-24 16:29:28 -0500

    Adds CatergoryCollectionViewCell.swift file to project

--- quavi/quavi/Supporting Files/SceneDelegate.swift
+++ quavi/quavi/Supporting Files/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = QuaviTabBarController()
+        window?.rootViewController = UINavigationController(rootViewController: CategorySelectionViewController())
         window?.makeKeyAndVisible()
     }
 
---
+++ quavi/quavi/Views/CatergoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CatergoryCollectionViewCell.swift
+//  quavi
+//
+//  Created by Mr Wonderful on 2/24/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class CatergoryCollectionViewCell: UICollectionViewCell {
+    
+}
