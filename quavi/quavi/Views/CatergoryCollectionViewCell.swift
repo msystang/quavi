d665db23f439de138aa21977fc9da91ecc4f0379
aabudu16 <Aabudu16@yahoo.com>
2020-02-24 16:30:42 -0500

    Adds categoryLabel, chevronIcon, and containerView to CatergoryCollectionViewCell file

--- quavi/quavi/Views/CatergoryCollectionViewCell.swift
+++ quavi/quavi/Views/CatergoryCollectionViewCell.swift
@@ -9,5 +9,26 @@
 import UIKit
 
 class CatergoryCollectionViewCell: UICollectionViewCell {
-    
+    //MARK: -- Objects
+       lazy var categoryLabel:UILabel = {
+           let label = UILabel()
+           label.font = UIFont(name: "Verdana-Bold", size: 18)
+           label.text = "Category name"
+           label.adjustsFontSizeToFitWidth = true
+           label.textAlignment = .center
+           return label
+       }()
+       
+       lazy var chevronIcon:UIImageView = {
+           let icon = UIImageView()
+           icon.image = UIImage(systemName: "chevron.compact.right")
+           icon.tintColor = .black
+           return icon
+       }()
+       
+       lazy var containerView:UIView = {
+           let view = UIView()
+           view.backgroundColor = .blue
+           return view
+       }()
 }
