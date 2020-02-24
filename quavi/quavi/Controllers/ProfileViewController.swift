1d4812ca4be14a8b3a5765955f5f55dee8d25bad
Radharani Valongo <radpursuitmedia@gmail.com>
2020-02-24 17:51:42 -0500

    adds profileInfoView and adds everything except tableView as subview to that

--- quavi/quavi/Controllers/ProfileViewController.swift
+++ quavi/quavi/Controllers/ProfileViewController.swift
@@ -12,6 +12,12 @@ class ProfileViewController: UIViewController {
     
     //MARK: - Lazy Properties
     
+    lazy var profileInfoView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .brown
+        return view
+    }()
+    
     lazy var editProfileButton: UIButton = {
         var button = UIButton()
         button.setBackgroundImage(UIImage(systemName: "gear"), for: .normal)
@@ -42,6 +48,17 @@ class ProfileViewController: UIViewController {
         var label = UILabel()
         label.text = "dontworrybehappy@gmail.com"
         label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var favoritedToursView: UIView = {
+        var view = UIView()
+        return view
+    }()
+    
+    lazy var faveToursLabel: UILabel = {
+        var label = UILabel()
         return label
     }()
     
@@ -58,22 +75,27 @@ class ProfileViewController: UIViewController {
         setUpSubviews()
         setUpConstraints()
         miscSetUp()
-        
     }
     
     //MARK: - Function
     private func setUpSubviews() {
-        self.view.addSubview(editProfileButton)
-        self.view.addSubview(userImage)
-        self.view.addSubview(fullname)
-        self.view.addSubview(username)
+        self.view.addSubview(profileInfoView)
+        self.profileInfoView.addSubview(editProfileButton)
+        self.profileInfoView.addSubview(userImage)
+        self.profileInfoView.addSubview(fullname)
+        self.profileInfoView.addSubview(username)
+        self.profileInfoView.addSubview(email)
+        self.view.addSubview(favoritesTableView)
     }
     
     private func setUpConstraints() {
+        constrainProfileInfoView()
         constrainEditButton()
         constrainUserImage()
         constrainFullName()
         constrainUsername()
+        constrainEmail()
+        constrainTableView()
     }
     
     private func miscSetUp(){
@@ -82,14 +104,27 @@ class ProfileViewController: UIViewController {
         self.userImage.layer.masksToBounds = true
     }
     
+    private func setUpFontSizes() {
+        
+    }
     //MARK: - Constraints
+    
+    func constrainProfileInfoView(){
+        profileInfoView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            profileInfoView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            profileInfoView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            profileInfoView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            profileInfoView.heightAnchor.constraint(equalToConstant: 450)
+        ])
+    }
     
     
     func constrainEditButton() {
         editProfileButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            editProfileButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50),
-            editProfileButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10),
+            editProfileButton.topAnchor.constraint(equalTo: self.profileInfoView.topAnchor, constant: 50),
+            editProfileButton.trailingAnchor.constraint(equalTo: self.profileInfoView.trailingAnchor, constant: -10),
             editProfileButton.widthAnchor.constraint(equalToConstant: 30),
             editProfileButton.heightAnchor.constraint(equalToConstant: 30)
         ])
@@ -98,8 +133,8 @@ class ProfileViewController: UIViewController {
     func constrainUserImage() {
         userImage.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            userImage.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
-            userImage.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
+            userImage.topAnchor.constraint(equalTo: self.profileInfoView.topAnchor, constant: 100),
+            userImage.leadingAnchor.constraint(equalTo: self.profileInfoView.leadingAnchor, constant: 15),
             userImage.widthAnchor.constraint(equalToConstant: 150),
             userImage.heightAnchor.constraint(equalToConstant: 150)
         ])
@@ -108,9 +143,9 @@ class ProfileViewController: UIViewController {
     func constrainFullName() {
         fullname.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            fullname.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
+            fullname.topAnchor.constraint(equalTo: self.profileInfoView.topAnchor, constant: 100),
             fullname.leadingAnchor.constraint(equalTo: userImage.trailingAnchor, constant: 20),
-            fullname.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
+            fullname.trailingAnchor.constraint(equalTo: self.profileInfoView.trailingAnchor, constant: -20),
             fullname.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
@@ -118,11 +153,30 @@ class ProfileViewController: UIViewController {
     func constrainUsername() {
         username.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            username.topAnchor.constraint(equalTo: fullname.bottomAnchor, constant: 10),
-            username.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 20),
-            username.leadingAnchor.constraint(equalTo: userImage.trailingAnchor, constant: 20),
+            username.topAnchor.constraint(equalTo: fullname.bottomAnchor, constant: 5),
+            username.trailingAnchor.constraint(equalTo: self.profileInfoView.trailingAnchor, constant: 20),
+            username.leadingAnchor.constraint(equalTo: userImage.trailingAnchor, constant: -20),
             username.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
     
+    func constrainEmail() {
+        email.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            email.topAnchor.constraint(equalTo: username.bottomAnchor, constant: 5),
+            email.trailingAnchor.constraint(equalTo: self.profileInfoView.trailingAnchor, constant: 20),
+            email.leadingAnchor.constraint(equalTo: userImage.trailingAnchor, constant: -20),
+            username.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    func constrainTableView() {
+        favoritesTableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            favoritesTableView.topAnchor.constraint(equalTo: userImage.bottomAnchor, constant: 10),
+            favoritesTableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            favoritesTableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            favoritesTableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+    }
 }
