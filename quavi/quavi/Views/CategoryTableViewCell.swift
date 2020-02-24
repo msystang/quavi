a6dea1a5f99173c5af042aa982ef28a0751e040d
aabudu16 <Aabudu16@yahoo.com>
2020-02-24 13:07:57 -0500

    Adds CategoryTableViewCell to project

---
+++ quavi/quavi/Views/CategoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryTableViewCell.swift
+//  quavi
+//
+//  Created by Mr Wonderful on 2/24/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
