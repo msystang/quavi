f358e9fc54d8c15a4303cef9fd40d466d6c49e28
Radharani Valongo <radpursuitmedia@gmail.com>
2020-02-24 12:16:36 -0500

    adds lazy var objects to profile vc

--- quavi/quavi/Controllers/ProfileViewController.swift
+++ quavi/quavi/Controllers/ProfileViewController.swift
@@ -9,7 +9,39 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    //MARK: - Lazy Properties
+    
+    lazy var logoutButton: UIButton = {
+        var button = UIButton()
+        
+        return button
+    }()
+    
+    lazy var userImage: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "plus")
+        return imageView
+    }()
+    lazy var fullname: UILabel = {
+        var label = UILabel()
+        label.text = "Bob Marley"
+        return label
+    }()
+    
+    lazy var username: UILabel = {
+        var label = UILabel()
+        label.text = "tour_master"
+        return label
+    }()
+    
+    lazy var email: UILabel = {
+        var label = UILabel()
+        label.text = "dontworrybehappy@gmail.com"
+        return label
+    }()
+    
+    //MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
