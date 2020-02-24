f8f88683a5298bf8b1f0498c2e7709f9580a1d0b
Radharani Valongo <radpursuitmedia@gmail.com>
2020-02-24 15:57:32 -0500

    adds editProfileButton and userImageView + constraints to favorites vc

--- quavi/quavi/Controllers/ProfileViewController.swift
+++ quavi/quavi/Controllers/ProfileViewController.swift
@@ -12,15 +12,16 @@ class ProfileViewController: UIViewController {
     
     //MARK: - Lazy Properties
     
-    lazy var logoutButton: UIButton = {
+    lazy var editProfileButton: UIButton = {
         var button = UIButton()
-        
+        button.setBackgroundImage(UIImage(systemName: "gear"), for: .normal)
+        button.tintColor = .black
         return button
     }()
     
     lazy var userImage: UIImageView = {
         var imageView = UIImageView()
-        imageView.image = #imageLiteral(resourceName: "plus")
+        imageView.backgroundColor = .darkGray
         return imageView
     }()
     lazy var fullname: UILabel = {
@@ -41,11 +42,60 @@ class ProfileViewController: UIViewController {
         return label
     }()
     
+    lazy var favoritesTableView: UITableView = {
+        let tv = UITableView()
+        
+        return tv
+    }()
+    
     //MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        setUpSubviews()
+        setUpConstraints()
+        miscSetUp()
+        
     }
     
-
+    //MARK: - Function
+    private func setUpSubviews() {
+        self.view.addSubview(editProfileButton)
+        self.view.addSubview(userImage)
+    }
+    
+    private func setUpConstraints() {
+        constrainEditButton()
+        constrainUserImage()
+    }
+    
+    private func miscSetUp(){
+        self.view.backgroundColor = .white
+        self.userImage.layer.cornerRadius = 50
+        self.userImage.layer.masksToBounds = true
+    }
+    
+    //MARK: - Constraints
+    
+    
+    func constrainEditButton() {
+        editProfileButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            editProfileButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50),
+            editProfileButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10),
+            editProfileButton.widthAnchor.constraint(equalToConstant: 30),
+            editProfileButton.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    func constrainUserImage() {
+        userImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            userImage.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
+            userImage.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
+            userImage.widthAnchor.constraint(equalToConstant: 100),
+            userImage.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
 }
