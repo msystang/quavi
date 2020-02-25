8e9d99a2f37d683426d6370cbb0f9e603f54bc46
aabudu16 <Aabudu16@yahoo.com>
2020-02-25 10:23:20 -0500

    Moves CategorySelectionViewController dataSource func to CategorySelectionViewController+DataSource.swift file

--- quavi/quavi/Controllers/CategorySelectionViewController+DataSource.swift
+++ quavi/quavi/Controllers/CategorySelectionViewController+DataSource.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2020 Sunni Tang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension CategorySelectionViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identifier.categoryCell.rawValue, for: indexPath) as? CatergoryCollectionViewCell else {return UICollectionViewCell()}
+        
+        cell.layer.borderWidth = 1
+        cell.layer.borderColor = #colorLiteral(red: 0.2843827307, green: 0.6391303539, blue: 0.8293711543, alpha: 1)
+        cell.layer.cornerRadius = 10
+        cell.clipsToBounds = true
+        return cell
+    }
+}
+
+extension CategorySelectionViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 0, right: 5)
+        layout.minimumInteritemSpacing = 5
+        let virticalCellCGSize = CGSize(width: (collectionView.frame.size.width - 20), height: collectionView.frame.size.height / 6)
+        return virticalCellCGSize
+    }
+}
+
