3446c5c569c5c2ba888ecf28571a9232b56ec381
aabudu16 <Aabudu16@yahoo.com>
2020-02-25 10:16:44 -0500

    Moves willDisplay delegate func to CategorySelectionViewController+Delegate

--- quavi/quavi/Controllers/CategorySelectionViewController+Delegate.swift
+++ quavi/quavi/Controllers/CategorySelectionViewController+Delegate.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2020 Sunni Tang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+//MARK: -- Extension
+extension CategorySelectionViewController: UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        let transformation = CATransform3DTranslate(CATransform3DIdentity, -500, 0, 0)
+        cell.layer.transform = transformation
+        cell.alpha = 0.0
+
+        UIView.animate(withDuration: 0.5) {
+            cell.alpha = 1.0
+            cell.layer.transform = CATransform3DIdentity
+        }
+    }
+}
--- quavi/quavi/Controllers/CategorySelectionViewController.swift
+++ quavi/quavi/Controllers/CategorySelectionViewController.swift
@@ -44,20 +44,6 @@ class CategorySelectionViewController: UIViewController {
         navigationController?.navigationBar.topItem?.title = "Tour Categories"
     }
 }
-//MARK: -- Extension
-extension CategorySelectionViewController: UICollectionViewDelegate{
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        
-        let transformation = CATransform3DTranslate(CATransform3DIdentity, -500, 0, 0)
-        cell.layer.transform = transformation
-        cell.alpha = 0.0
-
-        UIView.animate(withDuration: 0.5) {
-            cell.alpha = 1.0
-            cell.layer.transform = CATransform3DIdentity
-        }
-    }
-}
 
 extension CategorySelectionViewController: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
