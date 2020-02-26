7dc95b1f0bb4315aba49420ea5fd277c7a6c80a2
aabudu16 <Aabudu16@yahoo.com>
2020-02-26 11:18:13 -0500

    Adds a POIInfoViewController+PulseAnimation.swift file to project

---
+++ quavi/quavi/Controllers/POI Info/POIInfoViewController+PulseAnimation.swift
@@ -0,0 +1,9 @@
+//
+//  POIInfoViewController+PulseAnimation.swift
+//  quavi
+//
+//  Created by Mr Wonderful on 2/26/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import Foundation
