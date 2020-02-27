c89fa277c6999496c203881fb9026a210fdcb72e
Radharani Valongo <radpursuitmedia@gmail.com>
2020-02-27 16:03:44 -0500

    adds tableViewcell for profile faves tableview, adds profile tableview extension file, organises edit profile vc files and profile vc files into different folders

---
+++ quavi/quavi/Controllers/Profile/ProfileVC/ProfileVC+TableView.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileVC+TableView.swift
+//  quavi
+//
+//  Created by Radharani Ribas-Valongo on 2/27/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import Foundation
---
+++ quavi/quavi/Views/Profile/FaveToursCell.swift
@@ -0,0 +1,26 @@
+//
+//  FaveToursCell.swift
+//  quavi
+//
+//  Created by Radharani Ribas-Valongo on 2/27/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class FaveToursCell: UITableViewCell {
+
+    //MARK: - Lazy Properties
+    
+    //MARK: - Inits
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Functions
+}
