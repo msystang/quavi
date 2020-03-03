cb3057475450cf5746ecdd39b9a46701ab0b340f
aglegaspi <lex83nyc@gmail.com>
2020-03-03 15:29:34 -0500

    add properties, update constraints, add text

--- quavi/quavi/Views/PopUp/Slider Views/POIPopUpAboutView.swift
+++ quavi/quavi/Views/PopUp/Slider Views/POIPopUpAboutView.swift
@@ -15,6 +15,8 @@ class POIPopUpAboutView: UIView {
     lazy var poiName: UILabel = {
         var label = UILabel()
         label.textColor = .black
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 25, weight: .bold)
         label.text = "Sorry, No Title Provided!"
         label.adjustsFontSizeToFitWidth = true
         return label
@@ -23,6 +25,7 @@ class POIPopUpAboutView: UIView {
     lazy var imageView: UIImageView = {
         var imageView = UIImageView()
         imageView.layer.cornerRadius = 20
+        imageView.clipsToBounds = true
         imageView.image = UIImage(named: "empireState2")
         return imageView
     }()
@@ -30,6 +33,8 @@ class POIPopUpAboutView: UIView {
     lazy var descriptionTextView: UITextView = {
         var textView = UITextView()
         textView.isEditable = false
+        textView.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        textView.text = "Quavi's intentions are to change how people explore the areas within and surrounding them. In a world full of information we yearn to have options for various categories while being able to share this information with others. Quavi solves this need by providing curated self guided tours for the everyday person and the ability to make and share tours. It's easy to use and fun interface will make you a stan automatically. Stay tuned for Quavi to take over the world of tourism by storm!"
         return textView
     }()
     
@@ -50,7 +55,7 @@ class POIPopUpAboutView: UIView {
         NSLayoutConstraint.activate([
             poiName.topAnchor.constraint(equalTo: topAnchor, constant: padding),
             poiName.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
-            poiName.trailingAnchor.constraint(equalTo: trailingAnchor, constant: padding),
+            poiName.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
             poiName.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
@@ -61,7 +66,7 @@ class POIPopUpAboutView: UIView {
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: poiName.bottomAnchor, constant: padding),
             imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
-            imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: padding),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
             imageView.heightAnchor.constraint(equalToConstant: 200)
         ])
     }
@@ -72,7 +77,7 @@ class POIPopUpAboutView: UIView {
         NSLayoutConstraint.activate([
             descriptionTextView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: padding),
             descriptionTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
-            descriptionTextView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: padding),
+            descriptionTextView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
             descriptionTextView.heightAnchor.constraint(equalToConstant: 500)
         ])
     }
