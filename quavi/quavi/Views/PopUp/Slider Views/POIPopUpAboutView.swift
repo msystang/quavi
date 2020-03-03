cf232aa4ccad61ed60b77ff2a4b287e322b1b3c2
aglegaspi <lex83nyc@gmail.com>
2020-03-03 15:01:43 -0500

    create POIPopUpAbout View

---
+++ quavi/quavi/Views/PopUp/Slider Views/POIPopUpAboutView.swift
@@ -0,0 +1,21 @@
+//
+//  POIPopUpAboutView.swift
+//  quavi
+//
+//  Created by Alex 6.1 on 3/2/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class POIPopUpAboutView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
