5f611ad3e58d5be66c0c5b356a5f330f7d5c8c06
aglegaspi <lex83nyc@gmail.com>
2020-03-07 08:33:11 -0500

    resequence constraints and format

--- quavi/quavi/Controllers/POI Info/POIInfoViewController+Constraints.swift
+++ quavi/quavi/Controllers/POI Info/POIInfoViewController+Constraints.swift
@@ -17,28 +17,49 @@ extension POIInfoViewController{
         view.addSubview(continueButton)
     }
     
-    func continueButtonConstraints() {
-        continueButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([continueButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant:  -20), continueButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20), continueButton.heightAnchor.constraint(equalToConstant: 100), continueButton.widthAnchor.constraint(equalToConstant: 100)])
-    }
-    
     func easterEggButtonConstraints() {
         easterEggButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([easterEggButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10), easterEggButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15), easterEggButton.widthAnchor.constraint(equalToConstant: 35), easterEggButton.heightAnchor.constraint(equalToConstant: 35)])
-    }
-    
-    func containerViewConstraints() {
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([containerView.topAnchor.constraint(equalTo: easterEggButton.bottomAnchor, constant: 30), containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor), containerView.heightAnchor.constraint(equalToConstant: view.frame.height * 0.65), containerView.widthAnchor.constraint(equalToConstant: view.frame.width * 0.95) ])
+        NSLayoutConstraint.activate([
+            easterEggButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            easterEggButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            easterEggButton.widthAnchor.constraint(equalToConstant: 35),
+            easterEggButton.heightAnchor.constraint(equalToConstant: 35)])
     }
     
     func pageControlConstraints() {
         pageControl.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([pageControl.bottomAnchor.constraint(equalTo: containerView.topAnchor), pageControl.widthAnchor.constraint(equalTo: containerView.widthAnchor), pageControl.heightAnchor.constraint(equalToConstant: 20)])
+        NSLayoutConstraint.activate([
+            pageControl.bottomAnchor.constraint(equalTo: containerView.topAnchor),
+            pageControl.widthAnchor.constraint(equalTo: containerView.widthAnchor),
+            pageControl.heightAnchor.constraint(equalToConstant: 20)])
     }
+    
+    
+    func containerViewConstraints() {
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: easterEggButton.bottomAnchor, constant: 30),
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.heightAnchor.constraint(equalToConstant: view.frame.height * 0.65),
+            containerView.widthAnchor.constraint(equalToConstant: view.frame.width * 0.95) ])
+    }
+    
     
     func likeButtonConstraints() {
         likeButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([likeButton.heightAnchor.constraint(equalToConstant: 50), likeButton.widthAnchor.constraint(equalToConstant: 50), likeButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: likeButton.frame.height / 2 ), likeButton.centerXAnchor.constraint(equalTo: containerView.centerXAnchor)])
+        NSLayoutConstraint.activate([
+            likeButton.heightAnchor.constraint(equalToConstant: 50),
+            likeButton.widthAnchor.constraint(equalToConstant: 50),
+            likeButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: likeButton.frame.height / 2 ),
+            likeButton.centerXAnchor.constraint(equalTo: containerView.centerXAnchor)])
+    }
+    
+    func continueButtonConstraints() {
+        continueButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            continueButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant:  -20),
+            continueButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            continueButton.heightAnchor.constraint(equalToConstant: 100),
+            continueButton.widthAnchor.constraint(equalToConstant: 100)])
     }
 }
