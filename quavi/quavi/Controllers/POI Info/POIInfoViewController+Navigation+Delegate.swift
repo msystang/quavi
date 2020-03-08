6f1f4a4b803d23667674aee9321830d97e80703b
aabudu16 <Aabudu16@yahoo.com>
2020-03-08 14:04:42 -0400

    Adds POIInfoViewController+Navigation+Delegate.swift file to project

---
+++ quavi/quavi/Controllers/POI Info/POIInfoViewController+Navigation+Delegate.swift
@@ -0,0 +1,9 @@
+//
+//  POIInfoViewController+Navigation+Delegate.swift
+//  quavi
+//
+//  Created by Mr Wonderful on 3/8/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import Foundation
