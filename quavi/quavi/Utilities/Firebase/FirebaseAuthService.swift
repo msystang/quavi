995fa6dc033ca6519bf37a2a388cb9d6f2744994
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-09 16:26:40 -0400

    creates generic firebaseAuthService class and singleton

--- quavi/quavi/Utilities/Firebase/FirebaseAuthService.swift
+++ quavi/quavi/Utilities/Firebase/FirebaseAuthService.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+class FirebaseAuthService {
+    static let manager = FirebaseAuthService()
+    
+    private let auth = Auth.auth()
+    
+    var currentUser: User? {
+        return auth.currentUser
+    }
+    
+    func createNewUser(email: String, password: String, completion: @escaping (Result<User,Error>) -> ()) {
+        auth.createUser(withEmail: email, password: password) { (result, error) in
+            if let createdUser = result?.user {
+                completion(.success(createdUser))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func updateUserFields(userName: String? = nil,photoURL: URL? = nil, completion: @escaping (Result<(),Error>) -> ()){
+        let changeRequest = auth.currentUser?.createProfileChangeRequest()
+        if let userName = userName {
+            changeRequest?.displayName = userName
+        }
+        if let photoURL = photoURL {
+            changeRequest?.photoURL = photoURL
+        }
+        changeRequest?.commitChanges(completion: { (error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
+        })
+    }
+    
+    func loginUser(email: String, password: String, completion: @escaping (Result<(), Error>) -> ()) {
+        auth.signIn(withEmail: email, password: password) { (result, error) in
+            if let user = result?.user {
+                completion(.success(()))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private init () {}
+}
