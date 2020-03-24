9509085c2bd33741549b46c74f4ebc8c332ab7c6
aglegaspi <lex83nyc@gmail.com>
2020-03-24 14:12:55 -0400

    set up tableview color

--- quavi/quavi/Utilities/UIDesign/UIDesign.swift
+++ quavi/quavi/Utilities/UIDesign/UIDesign.swift
@@ -10,6 +10,7 @@ import UIKit
 
 struct UIDesign {
     
+    
     static func setBackgroundColor(_ view: UIView) {
         view.backgroundColor = UIColor(red: 0.204, green: 0.200, blue: 0.196, alpha: 1.0)
     }
@@ -19,7 +20,8 @@ struct UIDesign {
         tabBar.barTintColor = UIColor(red: 0.204, green: 0.200, blue: 0.196, alpha: 1.0)
     }
     
-    
-    
+    static func setTableViewColor(_ tableView: UITableView) {
+        tableView.backgroundColor = UIColor(red: 0.204, green: 0.200, blue: 0.196, alpha: 1.0)
+    }
     
 }
