4eb215bee40702f333255f134491c67b38e7949c
aabudu16 <Aabudu16@yahoo.com>
2020-03-23 21:47:50 -0400

    makes a revert

--- quavi/quavi/Controllers/POI Info/POIInfoViewController.swift
+++ quavi/quavi/Controllers/POI Info/POIInfoViewController.swift
@@ -75,13 +75,6 @@ class POIInfoViewController: UIViewController {
         }
     }
     
-    var poiData:POI? {
-           didSet{
-               guard let poi = poiData else {return}
-
-           }
-       }
-    
     //MARK:-- Objects
     lazy var continueButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
