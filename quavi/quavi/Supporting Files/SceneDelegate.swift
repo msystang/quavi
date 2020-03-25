97187e0a342312b19dab427826d8ee78ae4a0445
Radharani Valongo <radpursuitmedia@gmail.com>
2020-03-25 14:58:44 -0400

    sets up scene delegate according to whether user is logged in or not

--- quavi/quavi/Supporting Files/SceneDelegate.swift
+++ quavi/quavi/Supporting Files/SceneDelegate.swift
@@ -23,11 +23,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
         
-//        if Auth.auth().currentUser != nil {
+        if Auth.auth().currentUser != nil {
+            window?.rootViewController = QuaviTabBarController()
+        } else {
             window?.rootViewController = LoginViewController()
-//        } else {
-//            window?.rootViewController = OnBoardingViewController()
-//        }
+        }
        
         window?.makeKeyAndVisible()
         
