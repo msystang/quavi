ba09d7f1d2b4c51b90b437f600f2cc6e7e11dcc3
aglegaspi <lex83nyc@gmail.com>
2020-03-25 14:27:21 -0400

    create static var for light grey

--- quavi/quavi/Utilities/UIDesign/UIDesign.swift
+++ quavi/quavi/Utilities/UIDesign/UIDesign.swift
@@ -11,6 +11,7 @@ import UIKit
 struct UIDesign {
     
     static let quaviDarkGrey = UIColor(red: 0.161, green: 0.122, blue: 0.122, alpha: 1.0)
+    static let quaviLightGrey = UIColor.systemGray2
     static let quaviOrange = UIColor(red: 0.930, green: 0.191, blue: 0.153, alpha: 1.0)
     static let quaviYellow = UIColor.systemYellow
     static let quaviWhite = UIColor(red: 1.000, green: 1.000, blue: 0.843, alpha: 1.0)
