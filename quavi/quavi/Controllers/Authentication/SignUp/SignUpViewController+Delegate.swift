d6b965a4641b595f5c9e8db64f4ffcd7457e1923
aabudu16 <Aabudu16@yahoo.com>
2020-03-26 11:20:47 -0400

    extends signupVC with UITableviewDelegate

--- quavi/quavi/Controllers/Authentication/SignUp/SignUpViewController+Delegate.swift
+++ quavi/quavi/Controllers/Authentication/SignUp/SignUpViewController+Delegate.swift
@@ -6,4 +6,11 @@
 //  Copyright © 2020 Sunni Tang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension SignUpViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
