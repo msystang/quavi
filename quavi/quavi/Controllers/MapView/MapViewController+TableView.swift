534dbd3e6436359d1543123bacd511e282bd025d
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-04-16 18:21:20 -0400

    adds note if no stops are loaded in tableview

--- quavi/quavi/Controllers/MapView/MapViewController+TableView.swift
+++ quavi/quavi/Controllers/MapView/MapViewController+TableView.swift
@@ -23,49 +23,68 @@ extension MapViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return poiForTour.count
+        if poiForTour.count == 0 {
+            return 1
+        } else {
+            return poiForTour.count
+        }
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         
         let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 70))
         
-        //TO-DO: SEPERATE INTO IT'S OWN FILE
+        if poiForTour.count != 0 {
+            //TO-DO: SEPERATE INTO IT'S OWN FILE
+            
+            let overLayView =  UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 70))
+            overLayView.backgroundColor = UIDesign.quaviYellow
+            let button = UIButton(frame: CGRect(x: 65, y: 0, width: self.view.frame.width - 90, height: 70))
+            button.setTitle(poiForTour[section].name, for: .normal)
+            button.titleLabel!.adjustsFontSizeToFitWidth = true
+            button.backgroundColor = UIDesign.quaviYellow
+            button.addTarget(self, action: #selector(tvCellSectionButtonPressed(sender:)), for: .touchDown)
+            button.setTitleColor(.black, for: .normal)
+            button.tag = section
+            
+            view.layer.cornerRadius = button.frame.height / 2
+            view.clipsToBounds = true
+            view.addSubview(overLayView)
+            view.addSubview(button)
+            
+            let sectionImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+            sectionImage.layer.cornerRadius = sectionImage.frame.height / 2
+            sectionImage.layer.masksToBounds = true
+            
+            //This is the kingfisher code for the section if you guys dont like the fade in, the just delete the options: [.transition(.fade(0.2))].. its an optional so its not required not will it mess with the code
+            let imgUrlLStr = poiForTour[section].tableViewImage
+            sectionImage.kf.indicatorType = .activity
+            sectionImage.kf.setImage(with: URL(string: imgUrlLStr), placeholder: UIDesign.placeholderImage, options: [.transition(.fade(0.2))])
+            
+            button.addSubview(sectionImage)
+            
+            let sectionHeaderArrows = UIImageView(image: UIImage(systemName: "chevron.compact.down"))
+            sectionHeaderArrows.tintColor = .black
+            button.addSubview(sectionHeaderArrows)
+            
+            constrainTVSectionArrow(view: view, sectionHeaderArrows: sectionHeaderArrows)
+            constrainTVSectionImage(button: button, sectionHeaderImage: sectionImage, view: view)
+            
+            view.backgroundColor = .clear
+            
+        } else {
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 70))
+            label.text = "Select a tour above to get started!"
+            label.textColor = UIDesign.quaviLightGrey
+            label.textAlignment = .center
+            label.adjustsFontSizeToFitWidth = true
+            
+            view.clipsToBounds = true
+            view.addSubview(label)
+            
+        }
         
-        let overLayView =  UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 70))
-               overLayView.backgroundColor = UIDesign.quaviYellow
-         let button = UIButton(frame: CGRect(x: 65, y: 0, width: self.view.frame.width - 90, height: 70))
-        button.setTitle(poiForTour[section].name, for: .normal)
-        button.titleLabel!.adjustsFontSizeToFitWidth = true
-        button.backgroundColor = UIDesign.quaviYellow
-        button.addTarget(self, action: #selector(tvCellSectionButtonPressed(sender:)), for: .touchDown)
-        button.setTitleColor(.black, for: .normal)
-        button.tag = section
         
-        view.layer.cornerRadius = button.frame.height / 2
-        view.clipsToBounds = true
-        view.addSubview(overLayView)
-        view.addSubview(button)
-        
-        let sectionImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
-        sectionImage.layer.cornerRadius = sectionImage.frame.height / 2
-        sectionImage.layer.masksToBounds = true
-        
-        //This is the kingfisher code for the section if you guys dont like the fade in, the just delete the options: [.transition(.fade(0.2))].. its an optional so its not required not will it mess with the code
-        let imgUrlLStr = poiForTour[section].tableViewImage
-        sectionImage.kf.indicatorType = .activity
-        sectionImage.kf.setImage(with: URL(string: imgUrlLStr), placeholder: UIDesign.placeholderImage, options: [.transition(.fade(0.2))])
-        
-        button.addSubview(sectionImage)
-        
-        let sectionHeaderArrows = UIImageView(image: UIImage(systemName: "chevron.compact.down"))
-        sectionHeaderArrows.tintColor = .black
-        button.addSubview(sectionHeaderArrows)
-        
-        constrainTVSectionArrow(view: view, sectionHeaderArrows: sectionHeaderArrows)
-        constrainTVSectionImage(button: button, sectionHeaderImage: sectionImage, view: view)
-        
-        view.backgroundColor = .clear
         return view
     }
     
